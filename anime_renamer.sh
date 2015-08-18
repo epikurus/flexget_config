dd6c5814a757483e984ae980e07f759e7bc656d5
jonybat <jonybat26@hotmail.com>
2015-08-18 18:35:21 +0100

    Variable renaming

--- anime_renamer.sh
+++ anime_renamer.sh
@@ -4,14 +4,14 @@
 # https://github.com/hartfelt/kiara/
 #
 #Basic Settings
-EXEC="/usr/local/bin/kiara"
-ARGS=" --organize --overwrite --brief"
-ANIMELIST="/var/tmp/anime_rename.txt"
+bin="/usr/local/bin/kiara"
+args="--organize --overwrite --brief"
+animeList="/var/tmp/anime_rename.txt"
 
 get_anime_list() {
   #Define the internal field separator to CR and LF so that the array elements are full lines
   IFS=$'\r\n'
-  array=( $(cat $ANIMELIST) )
+  array=( $(cat $animeList) )
   #Restore the IFS to bash default (space)
   IFS=$' '
 }
@@ -29,35 +29,35 @@ fi
 #...Since it does, do what must be done
 for line in ${!array[*]}; do
   #Move array line to tmp var to make code easier to read
-  TMP="${array[$line]}"
+  path="${array[$line]}"
   #Escape \, /, & and [] because of sed
-  TMP_SAFE=$(echo $TMP | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g' -e 's/\[/\\[/g' -e 's/\]/\\]/g')
+  path_safe=$(echo $path | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g' -e 's/\[/\\[/g' -e 's/\]/\\]/g')
 
   #Check if file exists or not, to avoid errors from the program and get more accurate condition checks later on
-  if [[ -e "$TMP" ]]; then
-      echo "Renaming: $TMP"
+  if [[ -e "$path" ]]; then
+      echo "Renaming: $path"
       #Call the program to rename the file
-      $EXEC $ARGS "$TMP"
+      $bin $args "$path"
 
       #Do things if it was renamed and if not
-      if [[ -e "$TMP" ]]; then
+      if [[ -e "$path" ]]; then
           echo "File still exists. Either Kiara failed or the file does not exist in aniDB yet"
           #TODO: Introduce some sort of number of tries/blacklist system
       else
           #File does not exist anymore, so it must have been renamed successfully
           echo "File renamed successfully"
           #Remove the file that was renamed from the anime list file
-          sed -i "/$TMP_SAFE/d" $ANIMELIST
+          sed -i "/$path_safe/d" $animeList
       fi
   else
       #File does not exist, remove it from the list file
       echo "File does not exist, removing entry."
       #Remove the file that was renamed from the anime list file
-      sed -i "/$TMP_SAFE/d" $ANIMELIST
+      sed -i "/$path_safe/d" $animeList
   fi
 done
 
 #Kill kiara backend, since it does nothing after renaming
-$EXEC --kill
+$bin --kill
 
 exit 0
