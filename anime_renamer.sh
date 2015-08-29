78f09207a1240498ddbb7a509de8aa8286a64659
jonybat <jonybat26@hotmail.com>
2015-08-29 02:07:17 +0100

    Major rewrite

--- anime_renamer.sh
+++ anime_renamer.sh
@@ -3,57 +3,61 @@
 # Uses kiara to rename anime files according to anidb.net:
 # https://github.com/hartfelt/kiara/
 #
-#Basic Settings
+###SETTINGS
 bin="/usr/local/bin/kiara"
 args="--organize --overwrite --brief"
-animeList="/var/tmp/anime_rename.txt"
-
-get_anime_list() {
-  #Define the internal field separator to CR and LF so that the array elements are full lines
-  IFS=$'\r\n'
-  array=( $(cat $animeList) )
-  #Restore the IFS to bash default (space)
-  IFS=$' '
-}
+animeListFile="/var/tmp/anime_rename.txt"
 
 ###MAIN EXECUTION
 
-get_anime_list
+#Define the internal field separator to CR and LF so that the animeList elements are full lines
+IFS=$'\r\n'
+animeList=( $(cat $animeListFile) )
+#Restore the IFS to bash default (space)
+IFS=$' '
 
-#Check if array has contents before anything...
-if [[ ${#array[@]} -eq 0 ]]; then
-  echo "No entries in file, nothing to do."
+#Check if animeList has contents before anything...
+if [[ ${#animeList[@]} -eq 0 ]]; then
+  echo "$(date '+%H:%M:%S') - No entries in file, nothing to do."
   exit 1
 fi
 
 #...Since it does, do what must be done
-for line in ${!array[*]}; do
-  #Move array line to tmp var to make code easier to read
-  path="${array[$line]}"
+for line in ${!animeList[*]}; do
+  #Move animeList line to tmp var to make code easier to read
+  animePath="${animeList[$line]}"
   #Escape \, /, & and [] because of sed
-  path_safe=$(echo $path | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g' -e 's/\[/\\[/g' -e 's/\]/\\]/g')
+  animePath_safe=$(echo $animePath | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g' -e 's/\[/\\[/g' -e 's/\]/\\]/g')
+  #Split the path in multiple vars for later use
+  animeDir=$(echo $animePath | grep -Eo '/.*/')
+  animeName=$(echo $animePath | grep -Po '[^/]*(?=- ep.*)')
+  animeId=$(echo $animePath | grep -Po '(?<=ep)[0-9]{1,3}')
 
   #Check if file exists or not, to avoid errors from the program and get more accurate condition checks later on
-  if [[ -e "$path" ]]; then
-      echo "Renaming: $path"
-      #Call the program to rename the file
-      $bin $args "$path"
+  if [[ -e "$animePath" ]]; then
+    echo "$(date '+%H:%M:%S %d/%m/%Y') - Renaming: $animePath"
+    #Call the program to rename the file
+    $bin $args "$animePath"
 
-      #Do things if it was renamed and if not
-      if [[ -e "$path" ]]; then
-          echo "File still exists. Either Kiara failed or the file does not exist in aniDB yet"
-          #TODO: Introduce some sort of number of tries/blacklist system
-      else
-          #File does not exist anymore, so it must have been renamed successfully
-          echo "File renamed successfully"
-          #Remove the file that was renamed from the anime list file
-          sed -i "/$path_safe/d" $animeList
-      fi
-  else
-      #File does not exist, remove it from the list file
-      echo "File does not exist, removing entry."
+    #Do things if it was renamed and if not
+    if [[ $(ls "$animeDir" | grep $animeId | grep -E "[e,E]pisode|[u,U]nknown") ]]; then
+      #Episode name contains the word episode or unknown, so it was not renamed properly
+      echo "$(date '+%H:%M:%S') - File was not renamed properly! File probably does not exist in aniDB yet."
+    elif [[ -e "$animePath" ]]; then
+      #Original file still exists, so it was not renamed
+      echo "$(date '+%H:%M:%S') - File still exists with the same name. Possible Kiara or file permissions error."
+    else
+      #File does not exist anymore, and does not match the first case, so it must have been renamed
+      echo "$(date '+%H:%M:%S') - File renamed successfully to: $(ls "$animeDir" | grep $animeId)"
       #Remove the file that was renamed from the anime list file
-      sed -i "/$path_safe/d" $animeList
+      sed -i "/$animePath_safe/d" "$animeListFile"
+    fi
+
+  else
+    #File does not exist, remove it from the list file
+    echo "$(date '+%H:%M:%S %d/%m/%Y') - File does not exist, removing entry: $animePath"
+    #Remove the file that was renamed from the anime list file
+    sed -i "/$animePath_safe/d" "$animeListFile"
   fi
 done
 
