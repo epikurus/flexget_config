084caa8dbde103e89c7141b8c81bcda36d4bcb88
jonybat <jonybat26@hotmail.com>
2017-01-11 17:49:26 +0000

    Add filter for anime type, ign tags, courtesy of spyagent

--- plugins/myanimelist.py
+++ plugins/myanimelist.py
@@ -21,53 +21,76 @@ class myanimelist(object):
             'user_id': {
                 'type': 'string'
             },
+            'type': {
+                'type': 'string',
+                'enum': ['shows', 'movies'],
+                'default': 'shows'
+            },
             'list': {
                 'type': 'string',
                 'enum': ['watching', 'plantowatch', 'both'],
-                                'default': 'both'
+                'default': 'both'
             }
         }
     }
 
-
-
     @cached('myanimelist', persist='2 hours')
     def on_task_input(self, task, config):
 
-      def get_anime(task, url):
-        log.verbose("Requesting %s" % url)
-        page = task.requests.get(url)
-        if page.status_code != 200:
-          raise plugin.PluginError("Unable to get MAL list. Either the list is private or does not exist")
-        soup = get_soup(page.text)
+        def get_anime_tr(anime_entry):
+            tr = anime_entry
+            while tr.name != 'tr':
+                tr = tr.parent
+            return tr
 
-        as_ = soup.find_all("a", class_="animetitle")
+        def is_show(anime_type):
+            return anime_type == 'TV' or anime_type == 'OVA'
+
+        def is_requested_type(tr):
+            anime_type = tr.findAll('td')[3::4][0].text.strip()
+            if config['type'] == 'shows':
+                return is_show(anime_type)
+            return not is_show(anime_type)
+
+        def is_ignored(tr):
+            tags = tr.findAll('td')[5::6][0].findAll("a")
+            ign_tags = [tag for tag in tags if tag.text == "ign"]
+            return len(ign_tags) > 0
+
+        def get_anime(task, url):
+            log.verbose("Requesting %s" % url)
+            page = task.requests.get(url)
+            if page.status_code != 200:
+                raise plugin.PluginError("Unable to get MAL list. Either the list is private or does not exist")
+            soup = get_soup(page.text)
+
+            anime_entries = soup.find_all("a", class_="animetitle")
+            entries = []
+            for anime_entry in anime_entries:
+                tr = get_anime_tr(anime_entry)
+                if not is_requested_type(tr) or is_ignored(tr):
+                    continue;
+                entry = Entry()
+
+                entry["title"] = anime_entry.find("span").text
+                entry["myanimelist_name"] = anime_entry.find("span").text
+                entry["url"] = "http://myanimelist.net" + anime_entry["href"]
+                entries.append(entry)
+
+            return entries
+
+        # Create entries by parsing MAL wishlist page html using beautifulsoup
+        url = 'http://myanimelist.net/animelist/%s' % config['user_id']
+
         entries = []
-        for a in as_:
-          entry = Entry()
-
-          entry["title"] = a.find("span").text
-          entry["myanimelist_name"] = entry["title"]
-          entry["url"] = "http://myanimelist.net" + a["href"]
-          entries.append(entry)
+        if config['list'] == ['watching'] or config['list'] == 'both':
+            entries += get_anime(task, url + "?status=1")
+        if config['list'] == ['plantowatch'] or config['list'] == 'both':
+            entries += get_anime(task, url + "?status=6")
 
         return entries
-
-
-      # Create entries by parsing MAL wishlist page html using beautifulsoup
-      url = 'http://myanimelist.net/animelist/%s' % config['user_id']
-
-      entries = []
-      if config['list'] == ['watching'] or config['list'] == 'both':
-        entries = entries + get_anime(task, url + "?status=1")
-      if config['list'] == ['plantowatch'] or config['list'] == 'both':
-        entries = entries + get_anime(task, url + "?status=6")
-
-      return entries
 
 
 @event('plugin.register')
 def register_plugin():
     plugin.register(myanimelist, 'myanimelist', api_ver=2, groups=['list'])
-
-
